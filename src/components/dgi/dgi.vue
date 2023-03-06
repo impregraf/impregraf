798e2b7906acd50f4f5ffe57f6d216724d1bef9f
fedelexlabs <federico.sanchez@lexartlabs.com>
2023-03-06 17:12:27 -0300

    dgi maquetado

--- src/components/dgi/dgi.vue
+++ src/components/dgi/dgi.vue
@@ -10,8 +10,11 @@ defineComponent({
 </script>
 
 <template>
-<div class="w-full h-full">
-	<p>Para sacar constancia en dgi revisar este documento o lea a continuación</p>
+<div class="mx-auto grid sm:py-32 lg:max-w-7xl lg:px-8">
+	<p>Para sacar constancia en dgi revisar este documento o lea a continuación:</p>
+	<div class="bg-gray-700 p-6 my-6 rounded">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae eros nec risus commodo congue. Suspendisse consectetur, felis eu consectetur mollis, est massa mollis velit, vel eleifend eros est sed dui. Donec consequat justo vel arcu iaculis, at volutpat sem tempus. Mauris leo lectus, imperdiet et diam quis, cursus laoreet felis. Nam rutrum dui quam, eu fringilla sapien scelerisque id. Aenean porttitor sagittis nulla, id dapibus lectus sodales id. Proin lobortis consectetur sem, quis suscipit sapien rhoncus quis. Mauris mollis dolor nisi. Donec egestas arcu eu lectus tempus fermentum. Duis varius libero eget lectus fermentum, sit amet condimentum neque rutrum. Nullam elementum porta lacus eget convallis. Fusce pretium sem a elit elementum accumsan. Praesent finibus quam id aliquam suscipit. Nullam ac tristique dolor.
+	</div>
 </div>
 
 </template>
